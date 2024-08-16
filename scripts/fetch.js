55e292b9b355e4e723f6ad0b9a28225336a42352
minjeongss <dotoriido@gmail.com>
2024-08-16 13:39:18 +0900

    feat: FAKE_API_FILE 적용하여 테스트 진행

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,7 +1,16 @@
+//fake fetch: dummy data를 활용
+import { FAKE_API_FILE } from "./FAKE_API_FILE.js";
+const fakeFetch = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ item: FAKE_API_FILE });
+    }, 1000);
+  });
+};
+
 //fetch
 const getData = () => {
-  return fetch("http://localhost:3000/api/search?query=aladdin")
-    .then((response) => response.json())
+  return fakeFetch()
     .then((data) => {
       return data.item;
     })
@@ -9,6 +18,15 @@ const getData = () => {
       console.error("API 요청 중 오류 발생:", error);
       return undefined;
     });
+  // return fetch("http://localhost:3000/api/search?query=aladdin")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     return data.item;
+  //   })
+  //   .catch((error) => {
+  //     console.error("API 요청 중 오류 발생:", error);
+  //     return undefined;
+  //   });
 };
 
 //fetch한 데이터 활용
